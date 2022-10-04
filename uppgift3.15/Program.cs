f08e373fd3394e26fe00b18ea14ca85b7bb368cb
ouo2u <abod98451@gmail.com>
2022-10-04 05:26:09 +0200

    Add project files.

---
+++ uppgift3.15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace uppgift3._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur många timmar tänker du köra bilen? max hyr är 950 " + " 80 kr per timme");
+            int timmar = int.Parse(Console.ReadLine());
+           
+            if (timmar > 11) 
+            {
+                Console.WriteLine("du kan tuvär hyra bilen för mer än 950 ");
+
+            }
+           
+                else if (timmar < 950)
+            {
+                Console.WriteLine("den totala priset är" + timmar * 80 + "kr");
+            }
+
+
+
+        }
+
+    }
+}
